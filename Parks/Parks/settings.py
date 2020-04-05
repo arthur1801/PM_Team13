9eebe162b7ff1c0712da40318f94c6c48dce1ba5
Arthur1801 <arthur1801@outlook.com>
2020-04-05 20:43:48 +0300

    park

--- Parks/Parks/settings.py
+++ Parks/Parks/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'login.apps.LoginConfig',
     'Parkapp.apps.ParkappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
