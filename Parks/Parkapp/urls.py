d979fcb5b477a1792697d60a9f6f17e8118c1fcf
shahako <shahako1@ac.sce.ac.il>
2020-04-24 05:05:06 -0700

    fix registrtion

--- Parks/Parkapp/urls.py
+++ Parks/Parkapp/urls.py
@@ -7,7 +7,6 @@ from Parkapp import views as v
 urlpatterns = [
     path('', views.home, name='home-url'),
     path("register/", v.register, name="register"),
-    #path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='Parkapp/login.html'), name='login-url'),
     path('logout/', auth_views.LogoutView.as_view(template_name='Parkapp/home.html'), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
@@ -15,5 +14,5 @@ urlpatterns = [
     path('username_change/', views.UsernameChangeView , name='username_change'),
     path('username_change/change_name', views.ChangeName , name='change_name'),
     path('sendmail/', auth_views.LoginView.as_view(template_name='Parkapp/sendmail.html'), name='sendmail'),
-    #ath('register/', auth_views.register.as_view(template_name='Parkapp/register.html'), name='register')
+
 ]
